cf23b62d4053d5c7e5a99bcf9b887a5ced9d4560
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2024-10-10 16:15:35 +0200

    trying-to-spanwn-robot

--- go1_ros2/src/unitree_gazebo/launch/normal.launch.py
+++ go1_ros2/src/unitree_gazebo/launch/normal.launch.py
@@ -8,8 +8,10 @@ from launch_ros.actions import Node
 from launch.actions import OpaqueFunction
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import SetEnvironmentVariable
+from launch.actions import SetEnvironmentVariable, AppendEnvironmentVariable
 from launch import LaunchContext
+
+import xacro
 
 def launch_setup(context, *args, **kwargs):
     rname_val = LaunchConfiguration('rname').perform(context)
@@ -21,6 +23,17 @@ def launch_setup(context, *args, **kwargs):
     world_path = PathJoinSubstitution([FindPackageShare('unitree_gazebo'), 'worlds', wname_val + '.world'])
     #print(world_path.perform(context))
 
+    robot_description_path = os.path.join(
+        get_package_share_directory('go1_description'),
+        'xacro',
+        'robot.xacro'
+    )
+    robot_description = xacro.process_file(
+        robot_description_path, mappings={'DEBUG': 'false'}
+    ).toxml()
+
+
+
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
     gzserver_cmd = IncludeLaunchDescription(
                         PythonLaunchDescriptionSource(
@@ -28,8 +41,37 @@ def launch_setup(context, *args, **kwargs):
                         ),
                 launch_arguments={'gz_args': ['-r -s -v4 ', world_path], 'on_exit_shutdown': 'true'}.items())
 
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v4 '}.items()
+    )
 
-    return [gzserver_cmd]
+    # Load URDF into parameter server
+    robot_description_node = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[
+                {'robot_description': robot_description},
+                {'publish_frequency': 1000.0},
+                {'use_sim_time': True},
+            ],
+        )
+
+    spawn_robot_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-entity', 'go1_robot',
+            '-topic', 'robot_description'
+        ],
+        output='both'
+        )
+
+
+    return [robot_description_node, gzserver_cmd, gzclient_cmd, spawn_robot_node]
 
 def generate_launch_description():
     wname_arg = DeclareLaunchArgument('wname', default_value='earth')
