ccb4bf682256c1a13541e08f89053cdeef46cf6e
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2024-10-21 18:17:05 +0200

    fixing-urdf

--- go1_ros2/src/unitree_gazebo/launch/normal.launch.py
+++ go1_ros2/src/unitree_gazebo/launch/normal.launch.py
@@ -2,14 +2,14 @@ import os
 
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution, PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.actions import OpaqueFunction, ExecuteProcess
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import SetEnvironmentVariable, AppendEnvironmentVariable
-from launch import LaunchContext
+from launch.actions import SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
 
 import xacro
 
@@ -19,7 +19,6 @@ def launch_setup(context, *args, **kwargs):
     world_init_z = LaunchConfiguration("world_init_z")
     world_init_heading = LaunchConfiguration("world_init_heading")
 
-    rname = LaunchConfiguration('rname').perform(context)
     wname = LaunchConfiguration('wname').perform(context)
 
     robot_description_path = os.path.join(get_package_share_directory('go1_description'), 'xacro', 'robot.xacro')
@@ -42,7 +41,7 @@ def launch_setup(context, *args, **kwargs):
     gzserver_cmd = LaunchDescription([
         ExecuteProcess(
             cmd=['gzserver', '--verbose', '-s',
-            'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path],
+            'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
             output='screen'
         ),
     ])
@@ -79,37 +78,6 @@ def launch_setup(context, *args, **kwargs):
         output='screen'
     )
 
-    """
-    joint_publisher_node = Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher',
-            parameters=[{'use_gui': True}],
-        )
-    """
-
-
-    """
-
-    joint_trajectory_effort_controller_cmd = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_group_effort_controller'],
-        output='screen'
-    )
-
-    joint_state_broadcaster_cmd = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    joint_state_controller_cmd = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_states_controller'],
-        output='screen',
-    )
-    """
-
     joint_states_controller_node = Node(
             package='controller_manager',
             executable='spawner',
@@ -132,8 +100,22 @@ def launch_setup(context, *args, **kwargs):
 
     return [robot_description_node, set_env_var_gazebo,
             gzclient_cmd, gzserver_cmd, spawn_robot_cmd,
-            joint_states_controller_node, joint_group_effort_controller_node
-            ]
+            RegisterEventHandler(
+                OnProcessExit(
+                    target_action = spawn_robot_cmd,
+                    on_exit = [
+                        TimerAction(
+                            period=10.0,
+                            actions=[
+                                joint_states_controller_node, joint_group_effort_controller_node
+
+                            ]
+                        ),
+
+                    ]
+                )
+            )
+                        ]
 
 def generate_launch_description():
     declare_wname = DeclareLaunchArgument('wname', default_value='earth')
@@ -141,7 +123,7 @@ def generate_launch_description():
 
     declare_world_init_x = DeclareLaunchArgument("world_init_x", default_value="0.0")
     declare_world_init_y = DeclareLaunchArgument("world_init_y", default_value="0.0")
-    declare_world_init_z = DeclareLaunchArgument("world_init_z", default_value="0.0")
+    declare_world_init_z = DeclareLaunchArgument("world_init_z", default_value="1.0")
     declare_world_init_heading = DeclareLaunchArgument("world_init_heading", default_value="0.0")
 
     declare_ros_control_file = DeclareLaunchArgument(
