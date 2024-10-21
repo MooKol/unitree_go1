a9e482cbde86ac75ccd1b5225a3847966b698afe
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2024-10-21 15:28:43 +0200

    added-champ-fixing-simulation-launch

--- go1_ros2/src/unitree_gazebo/launch/normal.launch.py
+++ go1_ros2/src/unitree_gazebo/launch/normal.launch.py
@@ -14,37 +14,19 @@ from launch import LaunchContext
 import xacro
 
 def launch_setup(context, *args, **kwargs):
-    rname_val = LaunchConfiguration('rname').perform(context)
-    wname_val = LaunchConfiguration('wname').perform(context)
+    world_init_x = LaunchConfiguration("world_init_x")
+    world_init_y = LaunchConfiguration("world_init_y")
+    world_init_z = LaunchConfiguration("world_init_z")
+    world_init_heading = LaunchConfiguration("world_init_heading")
 
-    robot_path = FindPackageShare(rname_val + '_description')
-    #print(robot_path.perform(context))
+    rname = LaunchConfiguration('rname').perform(context)
+    wname = LaunchConfiguration('wname').perform(context)
 
-    world_path = PathJoinSubstitution([FindPackageShare('unitree_gazebo'), 'worlds', wname_val + '.world'])
-    #print(world_path.perform(context))
+    robot_description_path = os.path.join(get_package_share_directory('go1_description'), 'xacro', 'robot.xacro')
+    world_path = PathJoinSubstitution([FindPackageShare('unitree_gazebo'), 'worlds', wname + '.world'])
+    links_config = os.path.join(get_package_share_directory('go1_config'), "config/links/links.yaml")
 
-    declare_ros_control_file = DeclareLaunchArgument(
-        "ros_control_file",
-        default_value=os.path.join(get_package_share_directory('go1_description'), "config/ros_control.yaml"),
-    )
-
-    robot_description_path = os.path.join(
-        get_package_share_directory('go1_description'),
-        'xacro',
-        'robot.xacro'
-    )
-    robot_description = xacro.process_file(
-        robot_description_path, mappings={'DEBUG': 'false'}
-    ).toxml()
-
-    joint_publisher_node = Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher',
-            parameters=[{'use_gui': True}],
-        )
-
-    # Load URDF into parameter server
+    robot_description = xacro.process_file(robot_description_path, mappings={'DEBUG': 'false'}).toxml()
     robot_description_node = Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -54,67 +36,126 @@ def launch_setup(context, *args, **kwargs):
                 {'publish_frequency': 1000.0},
                 {'use_sim_time': True},
             ],
-        )
-    gazebo_model_path = os.path.join(get_package_prefix("go1_description"), "share")
-
-
-
-    load_joint_trajectory_effort_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_group_effort_controller'],
-        output='screen'
     )
 
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_states_controller'],
-        output='screen',
-    )
-
-    gazebo_ros_pkg = get_package_share_directory('gazebo_ros')
-
-    # Launch Gazebo server (gzserver) with Gazebo Classic
+    set_env_var_gazebo = SetEnvironmentVariable("GAZEBO_MODEL_PATH", os.path.join(get_package_prefix("go1_description"), "share"))
     gzserver_cmd = LaunchDescription([
         ExecuteProcess(
-            cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_factory.so', world_path],
+            cmd=['gzserver', '--verbose', '-s',
+            'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path],
             output='screen'
         ),
     ])
 
-    # Launch Gazebo client (gzclient) for visualization
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(gazebo_ros_pkg, 'launch', 'gzclient.launch.py')
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
         )
     )
 
-    spawn_robot_node = Node(
+    spawn_robot_cmd = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
-            '-entity', 'go1_robot',
-            '-topic', '/robot_description'
+            '-entity',
+            'go1_robot',
+            '-topic',
+            '/robot_description',
+            "-robot_namespace",
+            "",
+            "-x",
+            world_init_x,
+            "-y",
+            world_init_y,
+            "-z",
+            world_init_z,
+            "-R",
+            "0",
+            "-P",
+            "0",
+            "-Y",
+            world_init_heading,
         ],
         output='screen'
     )
 
     """
+    joint_publisher_node = Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher',
+            parameters=[{'use_gui': True}],
+        )
+    """
 
-    load_joint_state_broadcaster = ExecuteProcess(
+
+    """
+
+    joint_trajectory_effort_controller_cmd = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_group_effort_controller'],
+        output='screen'
+    )
+
+    joint_state_broadcaster_cmd = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
              'joint_state_broadcaster'],
         output='screen'
     )
+
+    joint_state_controller_cmd = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_states_controller'],
+        output='screen',
+    )
     """
 
-    return [joint_publisher_node, robot_description_node, SetEnvironmentVariable("GAZEBO_MODEL_PATH", os.path.join(get_package_prefix("go1_description"), "share")), 
-            gzclient_cmd, gzserver_cmd, spawn_robot_node]
+    joint_states_controller_node = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_states_controller']
+            )
+
+    joint_group_effort_controller_node = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_group_effort_controller']
+            )
+
+
+    contact_sensor = Node(
+        package="unitree_gazebo",
+        executable="contact_sensor",
+        output="screen",
+        parameters=[{"use_sim_time": True}, links_config],
+    )
+
+    return [robot_description_node, set_env_var_gazebo,
+            gzclient_cmd, gzserver_cmd, spawn_robot_cmd,
+            joint_states_controller_node, joint_group_effort_controller_node
+            ]
 
 def generate_launch_description():
-    wname_arg = DeclareLaunchArgument('wname', default_value='earth')
-    rname_arg = DeclareLaunchArgument('rname', default_value='go1')
+    declare_wname = DeclareLaunchArgument('wname', default_value='earth')
+    declare_rname = DeclareLaunchArgument('rname', default_value='go1')
+
+    declare_world_init_x = DeclareLaunchArgument("world_init_x", default_value="0.0")
+    declare_world_init_y = DeclareLaunchArgument("world_init_y", default_value="0.0")
+    declare_world_init_z = DeclareLaunchArgument("world_init_z", default_value="0.0")
+    declare_world_init_heading = DeclareLaunchArgument("world_init_heading", default_value="0.0")
+
+    declare_ros_control_file = DeclareLaunchArgument(
+        "ros_control_file",
+        default_value=os.path.join(get_package_share_directory('go1_description'), "config/ros_control.yaml"),
+    )
 
     return LaunchDescription([
-        wname_arg, rname_arg,
+        declare_wname,
+        declare_rname,
+        declare_world_init_x,
+        declare_world_init_y,
+        declare_world_init_z,
+        declare_world_init_heading,
+        declare_ros_control_file,
         OpaqueFunction(function = launch_setup)
         ])
