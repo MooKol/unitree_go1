bcfbf3913609ad9bf80448b92fc786447cf2fe96
petropoulakispanagiotis <petropoulakispanagiotis@gmail.com>
2024-11-08 15:57:07 +0100

    camera-ok

--- build/go2_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/go1/Desktop/workspace/unitree_go1/unitree-go2-ros2/robots/descriptions/go2_description
-# Build directory: /home/go1/Desktop/workspace/unitree_go1/build/go2_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
