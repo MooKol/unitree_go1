72b02a434e8db703267749700f23853cc7ef61d2
petropoulakispanagiotis <petropoulakispanagiotis@gmail.com>
2024-12-03 17:44:16 +0100

    first-exp-ok

--- real/src/unitree_nav/unitree_nav/launch/realsense.launch.py
+++ real/src/unitree_nav/unitree_nav/launch/realsense.launch.py
@@ -3,6 +3,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
 
 def generate_launch_description():
     # Path to the realsense2_camera package's launch file
@@ -18,8 +19,15 @@ def generate_launch_description():
             launch_arguments={
                 "enable_depth": "false",
                 "pointcloud.enable": "true",
-                "rgb_camera.color_profile": "1920x1080x30",
+                "rgb_camera.color_profile": "640x480x30",
             }.items(),
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher',
+            output='screen',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '1.0', '0.0', 'realsense_camera_link', 'camera_link']
         )
     ])
 
