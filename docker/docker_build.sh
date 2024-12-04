316def7ac35e114e9fc63e3e0f253a7c4610672e
petropoulakis <ge26kod@mytum.de>
2024-12-04 16:11:46 +0100

    added-ros2-communication

---
+++ docker/docker_build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Set Contianer Name
+CONTAINER_NAME="ros2_container"
+
+echo "Building ROS2-Humble Container"
+docker build --rm --progress plain -t $CONTAINER_NAME:latest .
+
+echo "Docker Build Completed"
